025554ba07beda2362a494dd40c206ccf86102f7
falcon-it <ilya.sokolov@live.ru>
2018-04-07 09:14:44 +0300

    +

--- web/index.php
+++ web/index.php
@@ -2,19 +2,6 @@
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
-/*$app = new Silex\Application();
-
-$app->get('/', function() use($app) {
-    return 'Hello, World!';
-    
-    # можно вернуть объект ответа, как и в Symfony
-    # return new Symfony\Component\HttpFoundation\Response('Hello, world');
-});
-
-$app->run();
-*/
-
 $bootstrap = new \Application\Bootstrap();
-
 $bootstrap->run();
         
